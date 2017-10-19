1b1d6240ec185e7c8a7ae64d44c61e96047f5593
Kevin Jaramillo <k.e.jl@hotmail.com>
2017-10-19 18:43:03 -0500

    Proyecto suma github

---
+++ suma.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+	int n1, n2;
+	cout<<"Escribe el numero 1: "<<endl;
+	cin>>n1;
+	cout<<"Escribe el numero 2: "<<endl;
+	cin>>n2;
+	cout<<"La suma de "<<n1<<" + "<<n2<<" es: "<<n1+n2;
+}
